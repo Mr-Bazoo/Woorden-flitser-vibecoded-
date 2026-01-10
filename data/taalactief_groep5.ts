03ce65d530ba0528ad929bd686a9bb8357f44080
Mr.Bas <80356267+Mr-Bazoo@users.noreply.github.com>
2026-01-10 19:17:51 +0100

    refactor: Remove SQL.js dependency for data
    Replaces the SQL.js database implementation with static TypeScript files for word lists. This simplifies data management and removes an external dependency.

---
+++ data/taalactief_groep5.ts
@@ -0,0 +1,10 @@
+export default [
+  {
+    "name": "Thema 1: De vakantie",
+    "words": ["zwemmen", "strand", "koffer", "reis", "auto", "vliegtuig"]
+  },
+  {
+    "name": "Thema 2: De school",
+    "words": ["schrift", "pen", "bord", "juf", "meester", "plein"]
+  }
+];
---
+++ data/vll_kim_groep3.ts
@@ -0,0 +1,14 @@
+export default [
+  {
+    "name": "Kern Start",
+    "words": ["ik", "kim", "sim", "kip", "aap", "rem"]
+  },
+  {
+    "name": "Kern 1",
+    "words": ["kip", "aap", "raak", "rem", "vis", "mis"]
+  },
+  {
+    "name": "Kern 2",
+    "words": ["tast", "mis", "maan", "roos", "vis", "sok"]
+  }
+];
